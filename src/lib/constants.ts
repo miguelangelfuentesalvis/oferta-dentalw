e0020bebd2bf5bfe8be5281ac3ce1f41d26a496b
Miguel Angel Fuentes Alvis <mafagrowthdigital@gmail.com>
2025-11-20 23:03:11 +0000

    todo salio bien solo hay que cambiar el mensaje de Whastapp por este
    Ho

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const WHATSAPP_PROMO_URL =
-  'https://wa.me/56967888588?text=Hola%2C%20estoy%20interesado%2Fa%20en%20la%20promoción%20de%20Black%20Friday%20del%20Plan%20Dúo%20de%20Prevención%20W%20por%20%2439.990.%20%C2%BFPodrían%20darme%20más%20información%3F';
+  'https://wa.me/56967888588?text=Hola%2C%20me%20interesa%20la%20promocio%CC%81n%20del%20Black%20Friday%20de%20estos%20di%CC%81as.%20Limpieza%20%2B%20Evaluacio%CC%81n%2C%20y%20quiza%CC%81s%20flu%CC%81or.%20Me%20indica%20cual%20es%20la%20sucursal%20ma%CC%81s%20cercana%3F%20Que%20di%CC%81a%20tiene%20disponible%3F';
