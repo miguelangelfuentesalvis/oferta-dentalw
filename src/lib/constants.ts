bc3fa3d36d1eb76a58de8517626179a7a4457ded
Miguel Angel Fuentes Alvis <mafagrowthdigital@gmail.com>
2025-11-20 22:34:34 +0000

    ahora acomoda el mensaje de los botones por este
    Hola, estoy interesad

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const WHATSAPP_PROMO_URL =
-  'https://wa.me/56967888588?text=Hola%2C%20vengo%20por%20la%20promoción%20Black%20Friday%20del%20Plan%20Dúo%20de%20Prevención%20W.%20Quisiera%20comprar%20mi%20voucher.';
+  'https://wa.me/56967888588?text=Hola%2C%20estoy%20interesado%2Fa%20en%20la%20promoción%20de%20Black%20Friday%20del%20Plan%20Dúo%20de%20Prevención%20W%20por%20%2439.990.%20%C2%BFPodrían%20darme%20más%20información%3F';
