9c9ae5a8aff9577664caaacaa4569627dc813379
Miguel Angel Fuentes Alvis <mafagrowthdigital@gmail.com>
2025-11-22 20:36:11 +0000

    cambia el texto pde Whastapp poe este
    Hola, me interesa la promoción d

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const WHATSAPP_PROMO_URL =
-  'https://wa.me/56967888588?text=Hola%2C%20me%20interesa%20la%20promoci%C3%B3n%20de%20la%20Black%20Week.%20Quisiera%20saber%20m%C3%A1s%20sobre%20el%20Plan%20Preventivo.%20Me%20indica%20cual%20es%20la%20sucursal%20m%C3%A1s%20cercana%3F%20Que%20d%C3%ADa%20tiene%20disponible%3F';
+  'https://wa.me/56967888588?text=Hola%2C%20me%20interesa%20la%20promoci%C3%B3n%20del%20Black%20Week.%20Quisiera%20saber%20m%C3%A1s%20sobre%20el%20Plan%20Preventivo.%20Me%20indica%20cual%20es%20la%20sucursal%20m%C3%A1s%20cercana%3F%20Que%20d%C3%ADa%20tiene%20disponible%3F';
 
 export const EMAIL_ADDRESS = 'recepcion@dentalw.cl';
